bfbe14e81d22dc5ef13e7dabb7e0ee0a30677a0f
shangyc <1041837742@qq.com>
2017-06-20 23:06:29 +0800

    正式提交goods;seller,ratings

---
+++ src/components/common/header.vue
@@ -0,0 +1,119 @@
+<template>
+	<header>
+		<div class="header">
+			<div class="header-left">
+				<img src="../../assets/headerimg/seller_avatar_256px.jpg"/>
+			</div>
+			<div class="header-right">
+				<ul>
+					<li>
+						<img src="../../assets/headerimg/brand@2x.png" class="imgpp"/>
+						<h3>粥品香坊（大运春）</h3>
+					</li>
+					<li>
+						<span>蜂鸟专送</span>/<span>38分钟送达</span>
+					</li>
+					<li class="threeLi">
+						<img src="../../assets/headerimg/decrease_1@3x.png"class="imgj"/>
+						<p>
+							<span>在线支付满28减5，满50减10</span>
+							</span>
+						</p>
+					</li>
+				</ul>
+		    </div>
+			<div class="gonggao">
+				<img src="../../assets/headerimg/bulletin@2x.png"/>
+				<span>粥品香坊其烹饪粥料的秘方源于中国千年古法，在融和现代制作工艺，由世界烹饪大师屈浩先生领衔研发。坚守纯天然、0添加的良心品质深得消费者青睐，发展至今成为粥类的引领品牌。是2008年奥运会和2013年园博会指定餐饮服务商。</span>
+			</div>
+		</div>
+	</header>
+</template>
+<script>
+	export default{
+		
+	}
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+body{
+	padding: 0px;
+	margin: 0px;
+	.header{
+		color: rgb(255,255,255);
+		background: rgba(7, 17, 27, 0.5);
+		overflow: hidden;
+		.header-left{
+			padding: 0px;
+			float: left;
+			display: inline-block;
+			margin-top: 20px;
+			img{
+				margin-left: 20px;
+				width:70px;
+				height: 70px;
+				border-radius: 4px;
+			}
+		}
+		div ul{
+			height: 80px;
+			padding: 0px;
+			display: inline-block;
+			li{
+				margin: 0px;
+				font-size: 13px;
+			  list-style: none;
+			  h3{
+			  	font-size: 20px;
+			  	color: rgb(255,255,255);
+			  	font-weight: bold;
+			  	line-height: 36px;
+			  	font-family:"arial, helvetica, sans-serif";
+			  	display: inline-block;
+			  	margin: 0px;
+			  	padding: 0px;
+			  }
+			  .imgj{
+			  	width: 20px;
+			  	height: 20px;
+			  }
+			  .imgpp{
+			  	width: 30px;
+			  	height: 20px;
+			  }
+			  p{
+                 display: inline-block;
+                 color: rgb(255,255,255);			  	
+			  }
+			}
+			.threeLi{
+				font-size: 13px;
+			}
+		}
+		.header-right{
+			float: left;
+			margin-top: 0px;
+			margin-left: 20px;
+			display: inline-block;
+		}
+		.gonggao{
+			background-color: rgba(7, 17, 27, .8);
+			font-size: 15px;
+			line-height: 30px;
+			width: 100%;
+			padding: 0px 10px;
+			overflow: hidden;
+			height: 30px;
+			img{
+				margin-top: 5px;
+				width: 30px;
+				height: auto;
+			}
+			span{
+				position: relative;
+				top: -2px;
+			}
+		}
+	}
+		
+}
+</style>
---
+++ src/components/goods/goods.vue
@@ -0,0 +1,248 @@
+<template>
+	<div class="containt">
+		<div class="containt_left">
+			<ul>
+				<li>热销版</li>
+				<li>单人特色套餐</li>
+				<li>特色粥品</li>
+				<li>精选热菜</li>
+				<li>爽口凉菜</li>
+				<li>半成品</li>
+			</ul>
+		</div>
+		<div class="containt_right">
+			<!--展现分类；使用ul-->
+			<ul>
+				<li>
+					 <p  class="title">热销版</p>
+					 <!--显示每个分类产品，使用ul-->
+					 <ul class="show_col">
+						 <li>
+							 <div class="detail">
+							 <div class="detailImg">
+								 <img src="http://fuss10.elemecdn.com/c/cd/c12745ed8a5171e13b427dbc39401jpeg.jpeg?imageView2/1/w/750/h/750"/>
+							 </div>
+							 <dl class="description">
+								 <dt  class="dt1">皮蛋瘦肉粥</dt>
+								 <dt  class="dt2">咸粥</dt>
+								 <dt  class="dt3"><span>月售229份</span><span>好评100%</span></dt>
+								 <dt  class="dt4">￥10</dt>
+							 </dl>
+						 </div>
+						 <i></i>
+						 </li>
+						 <li>
+							 <div class="detail">
+							 <div class="detailImg">
+								 <img src="http://fuss10.elemecdn.com/c/cd/c12745ed8a5171e13b427dbc39401jpeg.jpeg?imageView2/1/w/750/h/750"/>
+							 </div>
+							 <dl class="description">
+								 <dt  class="dt1">皮蛋瘦肉粥</dt>
+								 <dt  class="dt2">咸粥</dt>
+								 <dt  class="dt3"><span>月售229份</span><span>好评100%</span></dt>
+								 <dt  class="dt4">￥10</dt>
+							 </dl>
+						 </div>
+						 <i></i>
+						</li>
+					 </ul>
+				 </li>
+				 <li>
+					 <p  class="title">单人特色套餐</p>
+					 <!--显示每个分类产品，使用ul-->
+					 <ul class="show_col">
+						 <li>
+							 <div class="detail">
+							 <div class="detailImg">
+								 <img src="http://fuss10.elemecdn.com/c/cd/c12745ed8a5171e13b427dbc39401jpeg.jpeg?imageView2/1/w/750/h/750"/>
+							 </div>
+							 <dl class="description">
+								 <dt  class="dt1">皮蛋瘦肉粥</dt>
+								 <dt  class="dt2">咸粥</dt>
+								 <dt  class="dt3"><span>月售229份</span><span>好评100%</span></dt>
+								 <dt  class="dt4">￥10</dt>
+							 </dl>
+						 </div>
+						 <i></i>
+						 </li>
+						 <li>
+							 <div class="detail">
+							 <div class="detailImg">
+								 <img src="http://fuss10.elemecdn.com/c/cd/c12745ed8a5171e13b427dbc39401jpeg.jpeg?imageView2/1/w/750/h/750"/>
+							 </div>
+							 <dl class="description">
+								 <dt  class="dt1">皮蛋瘦肉粥</dt>
+								 <dt  class="dt2">咸粥</dt>
+								 <dt  class="dt3"><span>月售229份</span><span>好评100%</span></dt>
+								 <dt  class="dt4">￥10</dt>
+							 </dl>
+						 </div>
+						 <i></i>
+						</li>
+					 </ul>
+				 </li>
+				 <li>
+					 <p  class="title">特色粥品</p>
+					 <!--显示每个分类产品，使用ul-->
+					 <ul class="show_col">
+						 <li>
+							 <div class="detail">
+							 <div class="detailImg">
+								 <img src="http://fuss10.elemecdn.com/c/cd/c12745ed8a5171e13b427dbc39401jpeg.jpeg?imageView2/1/w/750/h/750"/>
+							 </div>
+							 <dl class="description">
+								 <dt  class="dt1">皮蛋瘦肉粥</dt>
+								 <dt  class="dt2">咸粥</dt>
+								 <dt  class="dt3"><span>月售229份</span><span>好评100%</span></dt>
+								 <dt  class="dt4">￥10</dt>
+							 </dl>
+						 </div>
+						 <i></i>
+						 </li>
+						 <li>
+							 <div class="detail">
+							 <div class="detailImg">
+								 <img src="http://fuss10.elemecdn.com/c/cd/c12745ed8a5171e13b427dbc39401jpeg.jpeg?imageView2/1/w/750/h/750"/>
+							 </div>
+							 <dl class="description">
+								 <dt  class="dt1">皮蛋瘦肉粥</dt>
+								 <dt  class="dt2">咸粥</dt>
+								 <dt  class="dt3"><span>月售229份</span><span>好评100%</span></dt>
+								 <dt  class="dt4">￥10</dt>
+							 </dl>
+						 </div>
+						 <i></i>
+						</li>
+					 </ul>
+				 </li>
+				 <li>
+					 <p  class="title">精选热菜</p>
+					 <!--显示每个分类产品，使用ul-->
+					 <ul class="show_col">
+						 <li>
+							 <div class="detail">
+							 <div class="detailImg">
+								 <img src="http://fuss10.elemecdn.com/c/cd/c12745ed8a5171e13b427dbc39401jpeg.jpeg?imageView2/1/w/750/h/750"/>
+							 </div>
+							 <dl class="description">
+								 <dt  class="dt1">皮蛋瘦肉粥</dt>
+								 <dt  class="dt2">咸粥</dt>
+								 <dt  class="dt3"><span>月售229份</span><span>好评100%</span></dt>
+								 <dt  class="dt4">￥10</dt>
+							 </dl>
+						 </div>
+						 <i></i>
+						 </li>
+						 <li>
+							 <div class="detail">
+							 <div class="detailImg">
+								 <img src="http://fuss10.elemecdn.com/c/cd/c12745ed8a5171e13b427dbc39401jpeg.jpeg?imageView2/1/w/750/h/750"/>
+							 </div>
+							 <dl class="description">
+								 <dt  class="dt1">皮蛋瘦肉粥</dt>
+								 <dt  class="dt2">咸粥</dt>
+								 <dt  class="dt3"><span>月售229份</span><span>好评100%</span></dt>
+								 <dt  class="dt4">￥10</dt>
+							 </dl>
+						 </div>
+						 <i></i>
+						</li>
+					 </ul>
+				 </li>
+			</ul>
+		</div>
+	</div>
+</template>
+<script>
+	
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+   li{
+   	padding: 0px;
+   	margin: 0px;
+   	list-style: none;
+   }
+   ul{
+   	padding: 0px;
+   	margin: 0px;
+   }
+   div{
+   	padding: 0px;
+   	margin: 0px;
+   }
+   .containt{
+   	font-family: "微软雅黑";
+   	display: flex;
+   	.containt_left{
+   		flex-grow:1; 
+   		background-color:#F4F5F7;
+   		ul{
+   			padding:0px 20px 0px 0px;
+   			li{
+   				text-align: center;
+   				line-height: 30px;
+   				margin: 10px 0px 0px 10px;
+   				width: 70px;
+   				height: 60px;
+   			}
+   		}
+   		
+   	}
+   	.containt_right{
+   		flex-grow:9; 
+   		.title{
+   			margin: 0px;
+   			background-color:#F4F5F7;
+   			border-left:2px solid #DCE0E4;
+   			padding: 10px 0px 10px 20px;
+   		}
+   		.show_col{
+   			display:flex;  /*flex流动布局*/
+   			flex-direction: column-reverse; /*布局方向*/
+   			flex-wrap: wrap;/*换行属性*/
+		    align-items: flex-start;/*y轴上的对齐方式*/
+		    align-content: flex-start;/*多跟轴线对其方式*/
+		    justify-content:flex-start;/*主轴（x）上对齐方式*/
+   			li{
+   				padding: 0px;
+   				border-bottom: 1px solid #F4F5F7;
+   				flex-grow:1;
+   				width: 100%;
+		   		.detail{
+		   			display: flex;
+		   			.detailImg img{
+		   				margin: 10px 0px 0px 10px;
+		   				width: 70px;
+		   				height: 70px;
+		   			}
+		   			.description{
+		   				margin: 7px 0px 0px 0px;
+		   				padding: 0px 0px 0px 10px;
+		   				dt{
+		   					margin: 1px 0px;
+		   				}
+		   				.dt1{
+		   					font-size: 16px;
+		   				}
+		   				.dt2{
+		   					color:#80868A;
+		   				}
+		   					font-size: 14px;
+		   				.dt3{
+		   					color: #80868A;
+		   					font-size: 14px;
+		   				}
+		   				.dt4{
+		   					color: red;
+		   					font-weight: bold;
+		   				}
+		   				
+		   			}
+		   		}
+   			}
+   		}
+   		
+   	}
+   	
+   }
+</style>
